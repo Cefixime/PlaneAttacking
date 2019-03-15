13c89391d2661a4f9876362da58f27bf5c0f5919
Cefixime <1627889449@qq.com>
2019-03-15 23:02:54 +0800

    Refactor

--- Forms/Planes.cs
+++ Forms/Planes.cs
@@ -168,7 +168,7 @@ namespace Plane
         }
         public void Fire()
         {
-            SingleObject.GetSingle().AddGameObject(new EnemyBullet(this, 20 , 5));
+            SingleObject.GetSingle().AddGameObject(new EnemyBullet(this, 20 , 1));
         }
         public override void IsOver()
         {
--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 
 ## Game picture
 
+![Picture 1](https://github.com/Cefixime/PlaneAttacking/blob/master/PictureShot/20190315221435.png "shot")
+
+## Operation
+
+- Move the mouse to move.
+- Fire automaticly.
+- Top left corner display the score and life.
+
